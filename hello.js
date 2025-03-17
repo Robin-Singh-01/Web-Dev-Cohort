d2521d238d365f474ac8c47aabd7117c58f1b27e
Robin Singh <149875694+Robin-Singh-01@users.noreply.github.com>
2025-03-17 18:35:57 +0530

    js basics

---
+++ hello.js
@@ -0,0 +1,17 @@
+function printChai(){
+
+    console.log("Hello chai");
+
+}
+function bringBrush(){
+
+    console.log("Haanji le aaya brush");
+}
+
+function addTwo(first, second){
+    return first+second;
+}
+
+printChai();
+bringBrush();
+console.log(addTwo(299,839));
